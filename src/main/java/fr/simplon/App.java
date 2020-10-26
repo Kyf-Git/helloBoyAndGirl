7f2bcbb5d455f1408300540d85cd9d3c4f65c78b
Vivien Le Duc <leducvivien@gmail.com>
2020-10-26 13:39:54 +0100

    ajout tests

--- src/main/java/fr/simplon/App.java
+++ src/main/java/fr/simplon/App.java
@@ -9,9 +9,14 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public static String helloBoyAndGirl(String name) {
+        return "Hello " + name;
     }
 }
---
+++ src/test/java/fr/simplon/HelloGino.java
@@ -0,0 +1,18 @@
+package fr.simplon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class HelloGino {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
